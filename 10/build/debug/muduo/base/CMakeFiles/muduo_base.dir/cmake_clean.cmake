03c1ad66fb8cda4d818d26dae17b6ce859788c4f
FL-sky <yueguangzhuying@qq.com>
2021-07-06 20:00:36 +0800

    save0

---
+++ 10/build/debug/muduo/base/CMakeFiles/muduo_base.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../../lib/libmuduo_base.a"
+  "../../lib/libmuduo_base.pdb"
+  "CMakeFiles/muduo_base.dir/Timestamp.cc.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/muduo_base.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- 10/jmuduo/muduo/base/Timestamp.cc
+++ 10/jmuduo/muduo/base/Timestamp.cc
@@ -10,10 +10,10 @@
 
 using namespace muduo;
 
-BOOST_STATIC_ASSERT(sizeof(Timestamp) == sizeof(int64_t));
+BOOST_STATIC_ASSERT(sizeof(Timestamp) == sizeof(int64_t)); // 编译时断言
 
 Timestamp::Timestamp(int64_t microseconds)
-  : microSecondsSinceEpoch_(microseconds)
+    : microSecondsSinceEpoch_(microseconds)
 {
 }
 
@@ -22,7 +22,7 @@ string Timestamp::toString() const
   char buf[32] = {0};
   int64_t seconds = microSecondsSinceEpoch_ / kMicroSecondsPerSecond;
   int64_t microseconds = microSecondsSinceEpoch_ % kMicroSecondsPerSecond;
-  snprintf(buf, sizeof(buf)-1, "%" PRId64 ".%06" PRId64 "", seconds, microseconds);
+  snprintf(buf, sizeof(buf) - 1, "%" PRId64 ".%06" PRId64 "", seconds, microseconds);
   return buf;
 }
 
@@ -35,9 +35,9 @@ string Timestamp::toFormattedString() const
   gmtime_r(&seconds, &tm_time);
 
   snprintf(buf, sizeof(buf), "%4d%02d%02d %02d:%02d:%02d.%06d",
-      tm_time.tm_year + 1900, tm_time.tm_mon + 1, tm_time.tm_mday,
-      tm_time.tm_hour, tm_time.tm_min, tm_time.tm_sec,
-      microseconds);
+           tm_time.tm_year + 1900, tm_time.tm_mon + 1, tm_time.tm_mday,
+           tm_time.tm_hour, tm_time.tm_min, tm_time.tm_sec,
+           microseconds);
   return buf;
 }
 
@@ -53,4 +53,3 @@ Timestamp Timestamp::invalid()
 {
   return Timestamp();
 }
-
